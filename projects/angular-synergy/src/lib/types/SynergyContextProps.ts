0d2113a23dfb98393f82fcfbecc73a5af5295621
rafaelmayor® <rafaelmayor22@gmail.com>
2021-10-09 23:10:01 -0400

    update to version 0.0.3

--- projects/angular-synergy/src/lib/types/SynergyContextProps.ts
+++
@@ -1,6 +0,0 @@
-export interface SynergyContextProps<T> {
-  dispatch: (type: string, payload?: unknown) => void
-  commit: (type: string, payload?: unknown) => void
-  state: T
-  getters: Record<string, Function>
-}
--- projects/angular-synergy/src/public-api.ts
+++ projects/angular-synergy/src/public-api.ts
@@ -4,3 +4,6 @@
 
 export * from './lib/angular-synergy.service';
 export * from './lib/angular-synergy.module';
+export * from './lib/core/Store';
+export * from './lib/types/SynergyCreateStoreOptions';
+export * from './lib/types/SynergyStoreProps';
